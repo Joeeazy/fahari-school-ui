92a87c275ce82bc98ba951607ddd3c6dd59351bb
Joeeazy <joeeazy34@gmail.com>
2025-07-27 19:34:31 +0300

    Sidebar menu

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,3 +1,8 @@
+import Menu from "@/components/Menu";
+import Navbar from "@/components/Navbar";
+import Image from "next/image";
+import Link from "next/link";
+
 export default function DashboardLayout({
     children,
   }: Readonly<{
@@ -6,12 +11,17 @@ export default function DashboardLayout({
     return (
         <div className="h-screen flex">
           {/* LEFT */}
-          <div className="w-1/6 md:w-[8%] lg:w-[16%] xl:w-[14%] bg-red-500">
-            L
+          <div className="w-1/6 md:w-[8%] lg:w-[16%] xl:w-[14%] p-4">
+            <Link href="/" className="flex items-center justify-center lg:justify-start gap-2 ">
+            <Image src="/logo.png" alt="logo" width={32} height={32}/>
+            <span className="hidden lg:block">Fahari School</span>
+            </Link>
+            <Menu />
           </div>
           {/* Right */}
-          <div className="w-5/6 md:w-[92%] lg:w-[84%] xl:w-[86%] bg-blue-500">
-          R
+          <div className="w-5/6 md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll">
+          <Navbar />
+          {children}
           </div>
         </div>
     );
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,23 @@
+import Image from 'next/image'
 import React from 'react'
 
 export default function Navbar() {
   return (
-    <div>Navbar</div>
+    <div className='flex items-center justify-between p-4'>
+      {/* Left Searchbar */}
+      <div className='hidden md:flex'>
+        <Image src="/search.png" alt="Search Icon" width={14} height={14}/>
+        <input type="text" placeholder='search...' />
+      </div>
+      {/* {Right Icons} */}
+      <div className='flex items-center justify-center gap-4'>
+      <div className=' bg-white rounded-full w-7 h-7 flex items-center justify-center cursor-pointer'>
+        <Image src="/message.png" alt='message icon'  width={20} height={20}/>
+      </div>
+      <div className='bg-white rounded-full w-7 h-7 flex items-center justify-center cursor-pointer'>
+        <Image src="/announcement.png" alt='message icon'  width={20} height={20}/>
+      </div>
+      </div>
+    </div>
   )
 }
